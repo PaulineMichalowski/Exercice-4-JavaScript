45fe7acf0e32156a4bf1da78d3dfb12510cf01a2
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-10-24 12:28:56 +0200

    Création de l'exercice 4

---
+++ Exercice4.js
@@ -0,0 +1,6 @@
+function form1() {
+  var nom = document.getElementById('nom').value;
+  var prenom = document.getElementById('prenom').value;
+  var ville = document.getElementById('ville').value;
+  alert('Votre nom : ' + nom  + '\n' + 'Votre prénom : ' + prenom + '\n' + 'Votre ville : ' + ville)
+}
